e3a35e932496b9c614999a0259e46aba3d478f52
tobik3332-plugins <faktnevim174@gmail.com>
2026-08-20 22:01:18 +0200

    Update DragonEggPlugin.java

--- src/main/java/cz/craft/dragonegg/DragonEggPlugin.java
+++ src/main/java/cz/craft/dragonegg/DragonEggPlugin.java
@@ -1,7 +1,13 @@
 package cz.craft.dragonegg;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DragonEggPlugin extends JavaPlugin {
 
@@ -11,17 +17,13 @@ public class DragonEggPlugin extends JavaPlugin {
     public void onEnable() {
         saveDefaultConfig();
 
-        // Registrace příkazu
         Bukkit.getCommandMap().register("dragonegg", new DragonEggCommand(this));
-
-        // Registrace listeneru
         getServer().getPluginManager().registerEvents(new DragonEggListener(this), this);
 
-        // Spuštění úlohy pro kontroly (glow, void teleportace, částice)
         this.task = new DragonEggTask(this);
         this.task.runTaskTimer(this, 10L, 10L);
 
-        getLogger().info("DragonEggProtect byl úspěšně načten!");
+        getLogger().info("DragonEggProtect byl uspesne nacten!");
     }
 
     @Override
@@ -29,5 +31,55 @@ public class DragonEggPlugin extends JavaPlugin {
         if (task != null) {
             task.clearAllGlow();
         }
+    }
+
+    public void addPlacedEgg(Location loc) {
+        if (loc == null || loc.getWorld() == null) return;
+        List<String> list = getConfig().getStringList("data.placed-eggs");
+        String locStr = loc.getWorld().getName() + "," + loc.getBlockX() + "," + loc.getBlockY() + "," + loc.getBlockZ();
+        if (!list.contains(locStr)) {
+            list.add(locStr);
+            getConfig().set("data.placed-eggs", list);
+            saveConfig();
+        }
+    }
+
+    public void removePlacedEgg(Location loc) {
+        if (loc == null || loc.getWorld() == null) return;
+        List<String> list = getConfig().getStringList("data.placed-eggs");
+        String locStr = loc.getWorld().getName() + "," + loc.getBlockX() + "," + loc.getBlockY() + "," + loc.getBlockZ();
+        if (list.contains(locStr)) {
+            list.remove(locStr);
+            getConfig().set("data.placed-eggs", list);
+            saveConfig();
+        }
+    }
+
+    public List<Location> getPlacedEggs() {
+        List<Location> locs = new ArrayList<>();
+        List<String> list = getConfig().getStringList("data.placed-eggs");
+        boolean changed = false;
+
+        for (String s : new ArrayList<>(list)) {
+            String[] parts = s.split(",");
+            if (parts.length == 4) {
+                World w = Bukkit.getWorld(parts[0]);
+                if (w != null) {
+                    Location loc = new Location(w, Integer.parseInt(parts[1]), Integer.parseInt(parts[2]), Integer.parseInt(parts[3]));
+                    if (loc.getBlock().getType() == Material.DRAGON_EGG) {
+                        locs.add(loc);
+                    } else {
+                        list.remove(s);
+                        changed = true;
+                    }
+                }
+            }
+        }
+
+        if (changed) {
+            getConfig().set("data.placed-eggs", list);
+            saveConfig();
+        }
+        return locs;
     }
 }
